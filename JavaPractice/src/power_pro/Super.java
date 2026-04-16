083ff9759049cc78c294c7cfb54a5f958dec6688
Sai Pavan <aspr80744@gmail.com>
2026-04-16 11:13:08 +0530

    Updated super and this code

--- JavaPractice/src/power_pro/Super.java
+++ JavaPractice/src/power_pro/Super.java
@@ -1,10 +1,33 @@
 package power_pro;
+//super(); method will be invincible there 
+//this(); method will class the present class constructor
+//all the classes in java extends class super();
+class Oneplus5
+{
+	public Oneplus5()
+	{
+		System.out.println("It is 5");
+	}
+}
+class Oneplus6 extends Oneplus5
+{
+	public Oneplus6()
+	{
+		System.out.println("It is 6");
+	}
 
+	public Oneplus6(int i) {
+        this();
+		System.out.println("It is int");
+
+	}
+}
 public class Super {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Oneplus6 obj = new Oneplus6(2);
 
 	}
-
 }
+
+
